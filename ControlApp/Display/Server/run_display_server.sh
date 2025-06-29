959516e2d099bc1354739c1d59880ef84a25c871
Cremetoertchen0815 <Mircosoft_heck@web.de>
2025-06-29 20:55:27 +0200

    Update scripts to use for Boxi 2

--- ControlApp/Display/Server/run_display_server.sh
+++ ControlApp/Display/Server/run_display_server.sh
@@ -3,19 +3,17 @@
 # Path to your Python script
 PYTHON_SCRIPT="display_server.py"
 
-nmcli con up id Boxi
-
 while true; do
     # Wait until wifi is connected
-    until nmcli con up id Boxi; do
-        echo "Failed to bring up the connection, retrying in 3 seconds..."
-        sleep 3
+    until sudo nmcli con up id Boxi; do
+        echo "Failed to bring up the connection, retrying in 5 seconds..."
+        sleep 5
     done
 
     # Wait until the endpoint is available
     until curl -s -o /dev/null -w "%{http_code}" http://192.168.4.1:8080/api/ping | grep -q "200"; do
-        echo "Control app not available, retrying in 3 seconds..."
-        sleep 3
+        echo "Control app not available, retrying in 5 seconds..."
+        sleep 5
     done
 
     echo "Control app is up. Starting display server..."
